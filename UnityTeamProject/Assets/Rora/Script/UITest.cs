88271e18ce252a8a28d0843d69def562aad8d387
KwonYeongmin <kym60002@daum.net>
2021-09-28 16:06:17 +0900

    0928

---
+++ UnityTeamProject/Assets/Rora/Script/UITest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITest : MonoBehaviour
+{
+    public GameObject hpBar;
+    private Image hpBarImg;
+    public GameObject shieldBar;
+    private Image shieldBarImg;
+
+    void Start()
+    {
+        hpBarImg = hpBar.GetComponent<Image>();
+        shieldBarImg = shieldBar.GetComponent<Image>();
+
+        hpBarImg.fillAmount = 0.3f;
+        shieldBarImg.fillAmount = 0.3f;
+
+
+      
+            
+    }
+
+    void Update()
+    {
+        Vector2 pos= shieldBar.GetComponent<RectTransform>().position;
+        float posX = shieldBar.GetComponent<RectTransform>().position.x;
+        posX = hpBar.GetComponent<RectTransform>().position.x;
+        shieldBar.GetComponent<RectTransform>().position = new Vector2(posX, pos.y);
+    }
+}
